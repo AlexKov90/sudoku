6247006d894310ef685f810f2b6a6b2f5ffb5593
AlexKov90 <kavaleuski.alex@gmail.com>
2019-03-03 22:11:52 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = matrix[row][col]
+
+      if (cell === 0) {
+        matrix[row][col] = getCandidates(matrix, row, col);
+      }
+    }
+
+  }
+  return matrix;
+}
+
+
+function getCandidates(matrix, row, col) {
+  let rowCand = [];
+
+  const writtenNumbers = matrix[row].reduce(function (set, cell) {
+    return set.add(cell);
+  }, new Set());
+
+  const colSet = new Set();
+  for (let rowNumber = 0; rowNumber < 9; rowNumber++) {
+    colSet.add(matrix[rowNumber][col]);
+  }
+
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  const colCand = numbers.filter(function(number) {
+    return !colSet.has(number);
+  });
+
+  // if there is only one canditate just return it
+  if (colCand.size === 1) {
+    return [...colCand][0];
+  }
+
+  rowCand = numbers.filter(function(number) {
+    return !writtenNumbers.has(number);
+  }
+  )
+
+  if (rowCand.size === 1) {
+    return [...rowCand][0];
+  }
+
+  return rowCand;
 }
